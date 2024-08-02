e77ce471df593a61843084b6130d214291610e00
twoweekhee <myjjoo4758@icloud.com>
2024-08-02 13:02:07 +0900

    start

---
+++ src/main/java/com/test/replicationtest/ReplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.replicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/replicationtest/ReplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.replicationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
