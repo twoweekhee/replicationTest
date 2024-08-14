43f73100965f5bf75ea9edbc3fa2c53946b8164e
twoweekhee <myjjoo4758@icloud.com>
2024-08-14 16:16:04 +0900

    Refactoring

--- src/main/java/com/test/replicationtest/dbtest/DatabaseController.java
+++ src/main/java/com/test/replicationtest/dbtest/DatabaseController.java
@@ -3,6 +3,7 @@ package com.test.replicationtest.dbtest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,9 @@ public class DatabaseController {
     private final DatabaseService databaseService;
 
     @GetMapping("/source")
-    public String  source() {
+    public ResponseEntity<String>  source() {
         log.info(databaseService.addData());
-        return databaseService.addData();
+        return ResponseEntity.ok(databaseService.addData());
     }
 
     @GetMapping("/replica")
